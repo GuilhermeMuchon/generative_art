539eadeceda3e62234b66bc3be2ae99abdbf260b
Guilherme Afonso Muchon <guilherme.muchon@gmail.com>
2021-01-10 14:56:50 -0300

    Update sketch06.js

--- sketches-studies/sketch06.js
+++ sketches-studies/sketch06.js
@@ -8,26 +8,30 @@ let stars = [];
 let speed;
 let img;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
-  for (let i = 0; i < 800; i++) {
-    stars[i] = new Star();
-  }
+// function preload() {
+//     img = loadImage('https://imgur.com/a/opYXAFs');
+// }
 
-  loadImage('sketches-studies/assets/cockpit.png', img => {
-    image(img, 10, 10, windowWidth, windowHeight);
-  }); 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    
+    for (let i = 0; i < 800; i++) {
+        stars[i] = new Star();
+    }
+
+    createDiv('<img src="assets/cockpit.png" alt="Ship Cockpit">').size(windowWidth, windowHeight);
+    
+    // image(img, 10, 10, windowWidth, windowHeight);
 }
 
 function draw() {
-  speed = map(mouseY * 3, 1, height, 1, 50);
-  background(0);
-  translate(width / 2, height / 2);
-  for (let i = 0; i < stars.length; i++) {
-    stars[i].update();
-    stars[i].show();
-  }
+    speed = map(mouseY * 3, 1, height, 1, 50);
+    background(0);
+    translate(width / 2, height / 2);
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].update();
+        stars[i].show();
+    }
 }
 
 function Star() {
@@ -38,33 +42,33 @@ function Star() {
     this.red = random(0, 255);
     this.green = random(0, 255);
     this.blue = random(0, 255);
-  
+    
     this.update = function() {
-      this.z = this.z - speed;
-      if (this.z < 1) {
-        this.z = width;
-        this.x = random(-width, width);
-        this.y = random(-height, height);
-        this.pz = this.z;
-      }
+        this.z = this.z - speed;
+        if (this.z < 1) {
+            this.z = width;
+            this.x = random(-width, width);
+            this.y = random(-height, height);
+            this.pz = this.z;
+        }
     };
-  
+    
     this.show = function() {
-      fill(this.red, this.green, this.blue);
-      noStroke();
-  
-      var sx = map(this.x / this.z, 0, 1, 0, width);
-      var sy = map(this.y / this.z, 0, 1, 0, height);
-  
-      var r = map(this.z, 0, width, 16, 0);
-      ellipse(sx, sy, r, r);
-  
-      var px = map(this.x / this.pz, 0, 1, 0, width);
-      var py = map(this.y / this.pz, 0, 1, 0, height);
-  
-      this.pz = this.z;
-  
-      stroke(255);
-      line(px, py, sx, sy);
+        fill(this.red, this.green, this.blue);
+        noStroke();
+        
+        var sx = map(this.x / this.z, 0, 1, 0, width);
+        var sy = map(this.y / this.z, 0, 1, 0, height);
+        
+        var r = map(this.z, 0, width, 16, 0);
+        ellipse(sx, sy, r, r);
+        
+        var px = map(this.x / this.pz, 0, 1, 0, width);
+        var py = map(this.y / this.pz, 0, 1, 0, height);
+        
+        this.pz = this.z;
+        
+        stroke(255);
+        line(px, py, sx, sy);
     };
-  }
+}
