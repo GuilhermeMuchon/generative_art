1a497fb9ab089538dc191ef7f9b9ec41101ab29a
Guilherme Afonso Muchon <guilherme.muchon@gmail.com>
2022-03-19 10:35:49 -0300

    frame reorder

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 document.body.onload = listSketches;
 
 var sketchesList = [
-    { id: "01", src: "https://i.redd.it/cosm6mk0jg941.jpg", title: 'Sketch 01', alt: 'Sketch 01' },
-    { id: "02", src: "https://i.redd.it/cosm6mk0jg941.jpg", title: 'Sketch 02', alt: 'Sketch 02' },
+    { id: "02", src: "https://raw.githubusercontent.com/GuilhermeMuchon/generative_art/master/img/sketch02.png", title: 'Sketch 02', alt: 'Sketch 02' },
+    { id: "04", src: "https://raw.githubusercontent.com/GuilhermeMuchon/generative_art/master/img/sketch04.png", title: 'Sketch 04', alt: 'Sketch 04' },
     { id: "03", src: "https://i.redd.it/cosm6mk0jg941.jpg", title: 'Sketch 03', alt: 'Sketch 03' },
-    { id: "04", src: "https://i.redd.it/cosm6mk0jg941.jpg", title: 'Sketch 04', alt: 'Sketch 04' },
-    { id: "05", src: "https://i.redd.it/cosm6mk0jg941.jpg", title: 'Sketch 05', alt: 'Sketch 05' },
-    { id: "06", src: "https://i.redd.it/cosm6mk0jg941.jpg", title: 'Sketch 06', alt: 'Sketch 06' }
+    { id: "05", src: "https://raw.githubusercontent.com/GuilhermeMuchon/generative_art/master/img/sketch05.png", title: 'Sketch 05', alt: 'Sketch 05' },
+    { id: "01", src: "https://raw.githubusercontent.com/GuilhermeMuchon/generative_art/master/img/sketch01.png", title: 'Sketch 01', alt: 'Sketch 01' },
+    { id: "06", src: "https://raw.githubusercontent.com/GuilhermeMuchon/generative_art/master/img/sketch06.png", title: 'Sketch 06', alt: 'Sketch 06' }
 ];
 
 function listSketches() {
